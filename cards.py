254f7edd8151c2984619698ddb13af374fade1f1
Themelis Kalomoiros <themelis.kalomoiros@teutologic.com>
2021-04-02 13:45:03 +0200

    Ranks and suits are just two lists.

--- cards.py
+++ cards.py
@@ -1,25 +1,8 @@
-class Rank:
-    """Includes all the available playing cards for a Bilota game."""
-
-    def __init__(self):
-        self.ace = 'A'
-        self.ten = '10'
-        self.king = 'K'
-        self.queen = 'Q'
-        self.jack = 'J'
-        self.nine = '9'
-        self.eight = '8'
-        self.seven = '7'
+from stacks import Stack
 
 
-class Suit:
-    """Includes all the suits of the playing cards."""
-
-    def __init__(self):
-        self.diamonds = 'Diamonds'
-        self.clubs = 'Clubs'
-        self.hearts = 'Hearts'
-        self.spades = 'Spades'
+ranks = ['A', '10', 'K', 'Q', 'J', '9', '8', '7']
+suits = ['Diamonds', 'Clubs', 'Hearts', 'Spades']
 
 
 class Card:
@@ -30,4 +13,3 @@ class Card:
         self.suit = suit
 
 
-        
