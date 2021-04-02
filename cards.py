2cf0fb664d1890df423c58d934ab09ad6433737d
Themelis Kalomoiros <themelis.kalomoiros@teutologic.com>
2021-04-02 17:38:32 +0200

    Created get_card_points

--- cards.py
+++ cards.py
@@ -6,6 +6,37 @@ ranks = ['A', '10', 'K', 'Q', 'J', '9', '8', '7']
 suits = ['Diamonds', 'Clubs', 'Hearts', 'Spades']
 
 
+def get_card_points(card, suit_of_kozia):
+    if card.suit == suit_of_kozia:
+        if card.rank == ranks[0]:
+            return 11
+        elif card.rank == ranks[1]:
+            return 10
+        elif card.rank == ranks[2]:
+            return 4
+        elif card.rank == ranks[3]:
+            return 3
+        elif card.rank == ranks[4]:
+            return 2
+        else:
+            return 0
+    else:
+        if card.rank == ranks[0]:
+            return 11
+        elif card.rank == ranks[1]:
+            return 10
+        elif card.rank == ranks[2]:
+            return 4
+        elif card.rank == ranks[3]:
+            return 3
+        elif card.rank == ranks[4]:
+            return 20
+        elif card.rank == ranks[6]:
+            return 14
+        else:
+            return 0
+
+
 class Card:
     """A french-suited playing card."""
 
